d3f192589a200264af4c577f486e05ff1ad2ade0
sbicak20 <sbicak20@foi.hr>
2023-12-27 12:10:10 +0100

    Trained new model with 1500 spoofed and 1500 live images with HOG features. This model did 89.5% of samples correctly classified.

--- helpers/imagePath.py
+++ helpers/imagePath.py
@@ -1,6 +1,8 @@
+import numpy as np
 from tqdm import tqdm
 import os
 import cv2
+from skimage.io import imread, imsave
 
 spoof_folder_path = r"C:\Users\sebas\Documents\FaceSpoofsDatasets\zips\CelebA_Spoof_train\Data\spoofedimages2"
 live_folder_path = r"C:\Users\sebas\Documents\FaceSpoofsDatasets\zips\CelebA_Spoof_train\Data\liveimages2"
@@ -24,7 +26,7 @@ def GetLiveImagePaths():
             live_image_paths.append(live_image_path)
     return live_image_paths
 
-def SaveImages(images, folder_name):
+def SaveImages(images, output_folder ,folder_name):
     rOutputFolder = output_folder + f"\{folder_name}"
     if not os.path.exists(rOutputFolder):
         os.makedirs(rOutputFolder)
@@ -32,3 +34,13 @@ def SaveImages(images, folder_name):
         if image is not None:
             output_path = os.path.join(rOutputFolder, f"output_image_{i}.png")
             cv2.imwrite(output_path, image)
+
+
+def SaveImagesScikit(images, output_folder, folder_name):
+    r_output_folder = os.path.join(output_folder, folder_name)
+    if not os.path.exists(r_output_folder):
+        os.makedirs(r_output_folder)
+
+    for i, image in enumerate(images):
+        output_path = os.path.join(r_output_folder, f"output_image_{i}.png")
+        imsave(output_path, image)
--- new.py
+++ new.py
@@ -6,23 +6,58 @@ from skimage.transform import resize
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from skimage.feature import hog
 import joblib
-
+import matplotlib.pyplot as plt
+from helpers.imagePath import SaveImagesScikit, SaveImages
 
 input_dir = r"C:\Users\sebas\Documents\FaceSpoofsDatasets\zips\CelebA_Spoof_train\Data"
-categories = ["spoofedimagestest2", "liveimagestest2"]
+output_folder = r"C:\Users\sebas\Documents\FaceSpoofsDatasets\zips\CelebA_Spoof_train\Data\processedImages"
+categories = ["spoofedimagestest3", "liveimagestest3"]
+
+def ShowImage(image):
+    plt.imshow(image, cmap='gray')
+    plt.title('Processed Image')
+    plt.axis('on')
+    plt.show()
+
 
 data = []
 labels = []
+features = []
 for category_index, category in enumerate(categories):
+    hog_images = []
+    greyscale_images = []
+    resized_images = []
     for file in os.listdir(os.path.join(input_dir, category)):
         img_path = os.path.join(input_dir,category,file)
+        # greyscale
         img = imread(img_path, as_gray=True)
-        img = resize(img,(32,32))
-        data.append(img.flatten())
+        #ShowImage(img)
+        greyscale_images.append(img)
+
+        #resized
+        img = resize(img,(64,64))
+        #ShowImage(img)
+        resized_images.append(img)
+
+        features_per_image, hog_image = hog(img, orientations=8, pixels_per_cell=(10, 10), cells_per_block=(3, 3),
+                                            visualize=True)
+        features.append(features_per_image.flatten())
+        #data.append(img.flatten())
         labels.append(category_index)
 
-data = np.asarray(data)
+        #HOG
+        #ShowImage(hog_image)
+        hog_images.append(hog_image)
+
+    SaveImages(hog_images, output_folder, category + r"HOG")
+    SaveImages(hog_images, output_folder, category + r"Greyscale")
+    SaveImages(hog_images, output_folder, category + r"Resized")
+
+#data = np.asarray(data)
+
+data = np.asarray(features)
 lables = np.asarray(labels)
 
 # split the data
@@ -38,5 +73,5 @@ y_prediction = classifier.predict(x_test)
 score = accuracy_score(y_prediction, y_test)
 print('{}% of samples were correctly classified'.format(str(score * 100)))
 
-pickle.dump(classifier, open('./faceantispoofmodel1000.p', 'wb'))
-joblib.dump(classifier, 'faceantispoofmodel1000.joblib')
+pickle.dump(classifier, open('./faceantispoofmodel1500withHOG.p', 'wb'))
+joblib.dump(classifier, 'faceantispoofmodel1500withHOG.joblib')
